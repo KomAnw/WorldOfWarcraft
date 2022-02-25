df0e3aab1bbe632185ca73ca88cfe0157305b4c6
Anwell <komissrov2525@gmail.com>
2022-02-25 04:57:45 +0300

    created new readme, added env

--- src/store/commonThunk/apiThunk.js
+++ src/store/commonThunk/apiThunk.js
@@ -2,16 +2,19 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { API_TOKEN_URL } from 'constants/index.ts';
 
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
 export const getApiToken = createAsyncThunk(
   'token/getToken',
   async (thunkAPI) => {
     const params = new URLSearchParams();
     params.append('grant_type', 'client_credentials');
     params.append('client_id', '0b27257c45fc488dbcbd816de5614f63');
-    params.append('client_secret', 'RB0jrwp8J4jH075VRZEAOkyHmtRhsrlV');
+    params.append('client_secret', CLIENT_SECRET);
 
     try {
       const token = await axios.post(API_TOKEN_URL, params);
+      console.log(token.data);
       return token.data;
     } catch (error) {
       console.log(error);
